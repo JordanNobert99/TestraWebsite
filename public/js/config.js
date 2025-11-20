12dfee8f0fc79ea09edc6704acf06db4e3010944
Jordan <Jordannobert99@gmail.com>
2025-11-20 13:39:35 -0700

    login config

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,12 @@
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBTeR0DEf1UzphdDdco8JxCRsLb7E0WGVs",
+    authDomain: "testra-website.firebaseapp.com",
+    projectId: "testra-website",
+    storageBucket: "testra-website.firebasestorage.app",
+    messagingSenderId: "523864898796",
+    appId: "1:523864898796:web:830fb43980feda6608f9c9",
+    measurementId: "G-9NFYC08ZR1"
 };
 
 // Initialize Firebase
